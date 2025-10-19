68ef37d43dabf12908bfaad35a0c257e5062b616
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-10-19 11:55:39 +0100

    shoot function refined

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector(".grid")
-const results = document.querySelector(".results")
+const resultDisplay = document.querySelector(".results")
 const width = 15
 const aliensRemoved = []
 let currentShooterIndex = 202
@@ -21,13 +21,13 @@ const squares = Array.from(document.querySelectorAll('.grid div'))
 // console.log(squares)
 
 const alienInvaders = [
-    0,1,2,3,4,5,6,7,8,9,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
 ]
 
 function draw() {
-    for (let i =0; 0 < alienInvaders.length; i++) {
+    for (let i =0; i < alienInvaders.length; i++) {
         if (!aliensRemoved.includes(i))
         squares[alienInvaders[i]].classList.add('invader')
     }
@@ -43,7 +43,7 @@ function remove() {
     }
 }
 
-function moveShooter() {
+function moveShooter(e) {
     squares[currentShooterIndex].classList.remove('shooter')
     switch(e.key) {
         case 'ArrowLeft':
@@ -98,9 +98,8 @@ function moveInvaders () {
 }
 invadersId = setInterval(moveInvaders, 600)
 
-function shoot() {
+function shoot(e) {
     let laserId
-
     let currentLaserIndex = currentShooterIndex
 
     function moveLaser() {
@@ -121,9 +120,8 @@ function shoot() {
             aliensRemoved.push(alienRemoved)
             results++
             resultDisplay.innerHTML = results
-            console.log(aliensRemoved) 
+            // console.log(aliensRemoved) 
         }
-        
     }
 
     if (e.key === 'ArrowUp') {
@@ -132,9 +130,7 @@ function shoot() {
  }
 
 
-document.addEventListener('keydown', shoot) {
-
-}
+document.addEventListener('keydown', shoot) 
 
 
 
