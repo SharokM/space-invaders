1d654cd064c34cce0be02923a63f0708474c2117
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-10-21 19:05:47 +0100

    restart button - start position reset function

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ function newGame() {
     currentShooterIndex = 202
     isGoingRight = true
     direction = 1
+
+    alienInvaders.length = 0
+    alienInvaders.push(
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    )
 }
 
 // CONTROL FUNCTIONALITY 
